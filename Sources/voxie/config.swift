8bc437fd514bd9f2ba6b37b7162b7d04c1eb8303
harry <yelcat@gmail.com>
2024-07-02 17:46:36 +0800

    fix: config file path

--- Sources/voxie/config.swift
+++ Sources/voxie/config.swift
@@ -39,20 +39,16 @@ struct Config: Codable {
 
 func loadConfig() -> Config {
     let fileManager = FileManager.default
-    let currentDirectoryPath = fileManager.currentDirectoryPath
-    print("current dir: \(currentDirectoryPath)")
-
-    guard let filePath = Bundle.main.path(forResource: "Config", ofType: "toml") else {
-        log.error("config file Config.toml not found, current dir: \(currentDirectoryPath)")
-        exit(-1)
-    }
+    let currentDirectoryURL = fileManager.currentDirectoryPath
+    let configFileURL = currentDirectoryURL.appendingPathComponent("Config.toml")
+    log.info("config file url: \(configFileURL)")
 
     do {
-        let fileContent = try String(contentsOfFile: filePath, encoding: .utf8)
+        let fileContent = try String(contentsOfFile: configFileURL, encoding: .utf8)
         let config = try TOMLDecoder().decode(Config.self, from: fileContent)
         return config
     } catch {
-        log.error("error while reading config file \(filePath): \(error.localizedDescription)")
+        log.error("error while reading config file: \(error.localizedDescription)")
         exit(-1)
     }
 }
